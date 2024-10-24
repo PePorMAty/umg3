59936d32c2f2e4b76727e27e9f16dca18bec100f
PePorMAty <themranti3256@gmail.com>
2024-10-24 13:05:55 +0700

    upd form

--- js/form.js
+++ js/form.js
@@ -2,39 +2,9 @@ const popup = document.querySelector(".popup");
 const main = document.querySelector(".main");
 const footer = document.querySelector(".footer");
 const overlay = document.querySelector(".overlay");
-const topForm = document.querySelector(".popup-top__form");
+const detailsForm = document.querySelector(".popup-details__form");
 const orderForm = document.querySelector(".popup-order-form");
-
-overlay.addEventListener("click", function (e) {
-  popup.classList.remove("popup-active");
-  overlay.classList.remove("overlay-active");
-  header.classList.remove("blur");
-  main.classList.remove("blur");
-  footer.classList.remove("blur");
-  orderForm.classList.remove("popup-order-form-active");
-});
-popup.addEventListener("click", function (e) {
-  const topFormPopup = e.composedPath().includes(topForm);
-  const orderFormPopup = e.composedPath().includes(orderForm);
-  if (!topFormPopup && !orderFormPopup) {
-    popup.classList.remove("popup-active");
-    overlay.classList.remove("overlay-active");
-    header.classList.remove("blur");
-    main.classList.remove("blur");
-    footer.classList.remove("blur");
-    orderForm.classList.remove("popup-order-form-active");
-  }
-});
-
-const openPopup = () => {
-  window.scrollTo({ top: 0 });
-  topForm.classList.add("popup-top__form-active");
-  popup.classList.add("popup-active");
-  overlay.classList.add("overlay-active");
-  header.classList.add("blur");
-  main.classList.add("blur");
-  footer.classList.add("blur");
-};
+const contactForm = document.querySelector(".popup-contact__form");
 
 const obj = {
   companyName: "",
@@ -54,11 +24,40 @@ const obj = {
   },
 };
 
-const confirmOrder = () => {
-  const companyName = document.getElementById("companyName").value;
-  const contactName = document.getElementById("contactName").value;
-  const email = document.getElementById("email").value;
-  const phone = document.getElementById("phone").value;
+overlay.addEventListener("click", function (e) {
+  popup.classList.remove("popup-active");
+  overlay.classList.remove("overlay-active");
+  header.classList.remove("blur");
+  main.classList.remove("blur");
+  footer.classList.remove("blur");
+  orderForm.classList.remove("popup-order-form-active");
+});
+popup.addEventListener("click", function (e) {
+  const detailsFormPopup = e.composedPath().includes(detailsForm);
+  const orderFormPopup = e.composedPath().includes(orderForm);
+  const contactFormPopup = e.composedPath().includes(contactForm);
+  if (!detailsFormPopup && !orderFormPopup && !contactFormPopup) {
+    popup.classList.remove("popup-active");
+    overlay.classList.remove("overlay-active");
+    header.classList.remove("blur");
+    main.classList.remove("blur");
+    footer.classList.remove("blur");
+    orderForm.classList.remove("popup-order-form-active");
+    contactForm.classList.remove("popup-contact__form-active");
+  }
+});
+
+const openPopup = () => {
+  window.scrollTo({ top: 0 });
+  detailsForm.classList.add("popup-details__form-active");
+  popup.classList.add("popup-active");
+  overlay.classList.add("overlay-active");
+  header.classList.add("blur");
+  main.classList.add("blur");
+  footer.classList.add("blur");
+};
+
+const moveToOrderForm = () => {
   const material = document.getElementById("materialInput").value;
   const size = document.getElementById("size").value;
   const blueprint = document.getElementById("blueprint").value;
@@ -74,13 +73,10 @@ const confirmOrder = () => {
   const addressOutput = document.getElementById("addressOutput");
 
   window.scrollTo({ top: 0 });
-  topForm.classList.remove("popup-top__form-active");
+  detailsForm.classList.remove("popup-details__form-active");
+  contactForm.classList.remove("popup-contact__form-active");
   orderForm.classList.add("popup-order-form-active");
 
-  obj.companyName = companyName;
-  obj.contactName = contactName;
-  obj.email = email;
-  obj.phone = phone;
   obj.material = material;
   obj.size = size;
   obj.blueprint = blueprint;
@@ -95,12 +91,32 @@ const confirmOrder = () => {
   quantityOutput.textContent = `${quantity} шт`;
   sizeOutput.textContent = `${size} мм`;
   addressOutput.textContent = `г.${city}, ул.${street}`;
-
-  console.log(obj);
 };
 
-const oneMoreOrder = () => {
+const returnToDetailsForm = () => {
   window.scrollTo({ top: 0 });
-  topForm.classList.add("popup-top__form-active");
+  detailsForm.classList.add("popup-details__form-active");
   orderForm.classList.remove("popup-order-form-active");
+  contactForm.classList.remove("popup-contact__form-active");
+};
+
+const moveToContactForm = () => {
+  window.scrollTo({ top: 0 });
+  detailsForm.classList.remove("popup-details__form-active");
+  orderForm.classList.remove("popup-order-form-active");
+  contactForm.classList.add("popup-contact__form-active");
+};
+
+const confirmForm = () => {
+  const companyName = document.getElementById("companyName").value;
+  const contactName = document.getElementById("contactName").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("phone").value;
+
+  obj.companyName = companyName;
+  obj.contactName = contactName;
+  obj.email = email;
+  obj.phone = phone;
+
+  console.log(obj);
 };
